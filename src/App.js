85d775a23253d20063d181d5b2575488526b11d0
Ivan Kalinić <ivankalinic1998@gmail.com>
2021-08-30 18:00:13 +0200

    Initial commit with folder structure and first component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import FrontPage from "./modules/FrontPage";
+
+function App() {
+  return (
+    <div className="App">
+      <FrontPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/modules/FrontPage/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.scss";
+
+const FrontPage = () => {
+  return <div className="app-parent-container">Hello</div>;
+};
+
+export default FrontPage;
